2d000269e9207ab1baf2ac544b22fdc287c1ab67
Lewis <lewis@evans-zone.me.uk>
2018-02-10 10:14:06 +0000

    updated structure

---
+++ src/datagathering/DataGatheringCore.java
@@ -0,0 +1,19 @@
+package footsiebot.datagatheringcore;
+
+public class DataGatheringCore implements IDataGathering {
+  private WebScraper webScraper;
+  private NewsScraper newsScraper;
+
+  public ScrapeResult getData() {
+	return webScraper.scrape();
+  }
+
+  public Article[] getNews(String company) {
+	return newsScraper.scrapeNews(company);
+  }
+
+  public Article[] getNews(String[] companies) {
+	return newsScraper.scrapeNews(companies);
+  }
+
+}
---
+++ src/datagathering/NewsScraper.java
@@ -0,0 +1,15 @@
+package footsiebot.datagatheringcore;
+
+public class NewsScraper {
+
+  public NewsScraper() {}
+
+  public Article[] scrapeNews(String company) {
+	return null;
+  }
+
+  public Article[] scrapeNews(String[] company) {
+	return null;
+  }
+
+}
